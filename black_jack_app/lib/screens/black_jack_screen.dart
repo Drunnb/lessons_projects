edfc631cfa3fe96643934ea237864a7165d4eee5
Drunnb <drunnb@gmail.com>
2021-11-22 21:40:23 +0300

    second part

--- black_jack_app/lib/screens/black_jack_screen.dart
+++ black_jack_app/lib/screens/black_jack_screen.dart
@@ -24,58 +24,58 @@ class _BlackJackScreenState extends State<BlackJackScreen> {
   int dealerScore = 0;
 
   final Map<String, int> deckOfCards = {
-    "cards/2.1.png": 2,
-    "cards/2.2.png": 2,
-    "cards/2.3.png": 2,
-    "cards/2.4.png": 2,
-    "cards/3.1.png": 3,
-    "cards/3.2.png": 3,
-    "cards/3.3.png": 3,
-    "cards/3.4.png": 3,
-    "cards/4.1.png": 4,
-    "cards/4.2.png": 4,
-    "cards/4.3.png": 4,
-    "cards/4.4.png": 4,
-    "cards/5.1.png": 5,
-    "cards/5.2.png": 5,
-    "cards/5.3.png": 5,
-    "cards/5.4.png": 5,
-    "cards/6.1.png": 6,
-    "cards/6.2.png": 6,
-    "cards/6.3.png": 6,
-    "cards/6.4.png": 6,
-    "cards/7.1.png": 7,
-    "cards/7.2.png": 7,
-    "cards/7.3.png": 7,
-    "cards/7.4.png": 7,
-    "cards/8.1.png": 8,
-    "cards/8.2.png": 8,
-    "cards/8.3.png": 8,
-    "cards/8.4.png": 8,
-    "cards/9.1.png": 9,
-    "cards/9.2.png": 9,
-    "cards/9.3.png": 9,
-    "cards/9.4.png": 9,
-    "cards/10.1.png": 10,
-    "cards/10.2.png": 10,
-    "cards/10.3.png": 10,
-    "cards/10.4.png": 10,
-    "cards/J1.png": 10,
-    "cards/J2.png": 10,
-    "cards/J3.png": 10,
-    "cards/J4.png": 10,
-    "cards/Q1.png": 10,
-    "cards/Q2.png": 10,
-    "cards/Q3.png": 10,
-    "cards/Q4.png": 10,
-    "cards/K1.png": 10,
-    "cards/K2.png": 10,
-    "cards/K3.png": 10,
-    "cards/K4.png": 10,
-    "cards/A1.png": 11,
-    "cards/A2.png": 11,
-    "cards/A3.png": 11,
-    "cards/A4.png": 11,
+    "assets/cards/2.1.png": 2,
+    "assets/cards/2.2.png": 2,
+    "assets/cards/2.3.png": 2,
+    "assets/cards/2.4.png": 2,
+    "assets/cards/3.1.png": 3,
+    "assets/cards/3.2.png": 3,
+    "assets/cards/3.3.png": 3,
+    "assets/cards/3.4.png": 3,
+    "assets/cards/4.1.png": 4,
+    "assets/cards/4.2.png": 4,
+    "assets/cards/4.3.png": 4,
+    "assets/cards/4.4.png": 4,
+    "assets/cards/5.1.png": 5,
+    "assets/cards/5.2.png": 5,
+    "assets/cards/5.3.png": 5,
+    "assets/cards/5.4.png": 5,
+    "assets/cards/6.1.png": 6,
+    "assets/cards/6.2.png": 6,
+    "assets/cards/6.3.png": 6,
+    "assets/cards/6.4.png": 6,
+    "assets/cards/7.1.png": 7,
+    "assets/cards/7.2.png": 7,
+    "assets/cards/7.3.png": 7,
+    "assets/cards/7.4.png": 7,
+    "assets/cards/8.1.png": 8,
+    "assets/cards/8.2.png": 8,
+    "assets/cards/8.3.png": 8,
+    "assets/cards/8.4.png": 8,
+    "assets/cards/9.1.png": 9,
+    "assets/cards/9.2.png": 9,
+    "assets/cards/9.3.png": 9,
+    "assets/cards/9.4.png": 9,
+    "assets/cards/10.1.png": 10,
+    "assets/cards/10.2.png": 10,
+    "assets/cards/10.3.png": 10,
+    "assets/cards/10.4.png": 10,
+    "assets/cards/J1.png": 10,
+    "assets/cards/J2.png": 10,
+    "assets/cards/J3.png": 10,
+    "assets/cards/J4.png": 10,
+    "assets/cards/Q1.png": 10,
+    "assets/cards/Q2.png": 10,
+    "assets/cards/Q3.png": 10,
+    "assets/cards/Q4.png": 10,
+    "assets/cards/K1.png": 10,
+    "assets/cards/K2.png": 10,
+    "assets/cards/K3.png": 10,
+    "assets/cards/K4.png": 10,
+    "assets/cards/A1.png": 11,
+    "assets/cards/A2.png": 11,
+    "assets/cards/A3.png": 11,
+    "assets/cards/A4.png": 11,
   };
   Map<String, int> playingCards = {};
 
@@ -89,10 +89,41 @@ class _BlackJackScreenState extends State<BlackJackScreen> {
 
     Random random = Random();
 
+    // String randomCard() {
+    //   String cardOneKey =
+    //       playingCards.keys.elementAt(random.nextInt(playingCards.length));
+    //   return cardOneKey;
+    // }
+
     String cardOneKey =
         playingCards.keys.elementAt(random.nextInt(playingCards.length));
     //from 1 to playingCards.length inclusive
     playingCards.removeWhere((key, value) => key == cardOneKey);
+    String cardTwoKey =
+        playingCards.keys.elementAt(random.nextInt(playingCards.length));
+    playingCards.removeWhere((key, value) => key == cardTwoKey);
+    String cardThreeKey =
+        playingCards.keys.elementAt(random.nextInt(playingCards.length));
+    playingCards.removeWhere((key, value) => key == cardThreeKey);
+    String cardFourKey =
+        playingCards.keys.elementAt(random.nextInt(playingCards.length));
+    playingCards.removeWhere((key, value) => key == cardFourKey);
+
+    dealersFirstCard = cardOneKey;
+    dealersSecondsCard = cardTwoKey;
+    playesFirstCard = cardThreeKey;
+    playesSecondsCard = cardFourKey;
+
+    dealersCards.add(Image.asset(dealersFirstCard!));
+    dealersCards.add(Image.asset(dealersSecondsCard!));
+
+    dealerScore =
+        deckOfCards[dealersFirstCard]! + deckOfCards[dealersSecondsCard]!;
+
+    myCards.add(Image.asset(playesFirstCard!));
+    myCards.add(Image.asset(playesSecondsCard!));
+    playerScore =
+        deckOfCards[playesFirstCard]! + deckOfCards[playesSecondsCard]!;
   }
 
   void addCard() {}
@@ -115,13 +146,37 @@ class _BlackJackScreenState extends State<BlackJackScreen> {
                     Column(
                       children: [
                         Text('Dealer score $dealerScore'),
-                        SizedBox(height: 20),
+                        const SizedBox(height: 20),
+                        Container(
+                          height: 200,
+                          child: GridView.builder(
+                              itemCount: dealersCards.length,
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 3),
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemBuilder: (context, index) {
+                                return dealersCards[index];
+                              }),
+                        ),
                       ],
                     ),
                     Column(
                       children: [
                         Text('Player score $playerScore'),
-                        SizedBox(height: 20),
+                        const SizedBox(height: 20),
+                        Container(
+                          height: 200,
+                          child: GridView.builder(
+                              itemCount: myCards.length,
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 3),
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemBuilder: (context, index) {
+                                return myCards[index];
+                              }),
+                        ),
                       ],
                     ),
                     IntrinsicWidth(
