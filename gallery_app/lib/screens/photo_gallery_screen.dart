dd502b71a2e3285d6be3fa0776f060c3317c5b8f
Drunnb <drunnb@gmail.com>
2021-11-29 20:26:08 +0300

    make api response

--- gallery_app/lib/screens/photo_gallery_screen.dart
+++ gallery_app/lib/screens/photo_gallery_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gallery_app/keys.dart';
+import 'package:gallery_app/services/network_helper.dart';
 
 class PhotoGallery extends StatefulWidget {
   PhotoGallery({Key? key}) : super(key: key);
@@ -8,6 +10,25 @@ class PhotoGallery extends StatefulWidget {
 }
 
 class _PhotoGalleryState extends State<PhotoGallery> {
+  Future<List<String>>? images;
+  Future<List<String>> getImages() async {
+    List<String> images = [];
+    String url = 'https://pixabay.com/api/?key=$pixabyApiKey&image_type=prhoto';
+    NetworkHelper networkHelper = NetworkHelper(url: url);
+    dynamic data = await networkHelper.getData();
+    List<dynamic> hitsList = data['hits'] as List;
+    for (int i = 0; i < hitsList.length; i++) {
+      images.add(hitsList[i]['largeImageURL']);
+    }
+    return images;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    images = getImages();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ gallery_app/lib/services/network_helper.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  final String url;
+  NetworkHelper({required this.url});
+
+  Future<dynamic> getData() async {
+    http.Response response;
+    response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+      return;
+    }
+  }
+}
