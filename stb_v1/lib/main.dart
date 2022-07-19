8fb0691bf2d7e302773c6f19c69326717cc3fc25
Drunnb <drunnb@gmail.com>
2022-07-19 19:20:57 +0300

    second part will be sucsessfull

--- stb_v1/lib/main.dart
+++ stb_v1/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stb_v1/bloc/location_cubit.dart';
 import 'package:stb_v1/screens/map_screen.dart';
 
 void main() {
@@ -10,17 +12,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // Color.fromARGB(255, 54, 70, 92),
-        primarySwatch: Colors.blueGrey, // app bar color
-        colorScheme: const ColorScheme.light().copyWith(
-          primary: const Color(0xff548CA8), // textField color
-          secondary: const Color(0xff96BAFF), // Floating action button
+    return BlocProvider<LocationCubit>(
+      create: (context) => LocationCubit()..getLocation(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // Color.fromARGB(255, 54, 70, 92),
+          primarySwatch: Colors.blueGrey, // app bar color
+          colorScheme: const ColorScheme.light().copyWith(
+            primary: const Color(0xff548CA8), // textField color
+            secondary: const Color(0xff96BAFF), // Floating action button
+          ),
         ),
+        home: MapScreen(),
       ),
-      home: MapScreen(),
     );
   }
 }
--- stb_v1/lib/screens/map_screen.dart
+++ stb_v1/lib/screens/map_screen.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:stb_v1/bloc/location_cubit.dart';
 
 // ignore: use_key_in_widget_constructors
 class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FlutterMap(
-        options: MapOptions(
-          center: LatLng(0, 0),
-          zoom: 15.3,
-          maxZoom: 17,
-          minZoom: 3.5,
-        ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: ['a', 'b', 'c'],
-            retinaMode: true,
-          ),
-        ],
+      body: BlocBuilder<LocationCubit, LocationState>(
+        builder: ((context, state) {
+          if (state is LocationLoaded) {
+            return FlutterMap(
+              options: MapOptions(
+                center: LatLng(state.latitude, state.longitude),
+                zoom: 15.3,
+                maxZoom: 17,
+                minZoom: 3.5,
+              ),
+              layers: [
+                TileLayerOptions(
+                  urlTemplate:
+                      'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  subdomains: ['a', 'b', 'c'],
+                  retinaMode: true,
+                ),
+              ],
+            );
+          }
+          if (state is LocationError) {
+            return Center(
+              child: MaterialButton(
+                  child: const Text('Try again'),
+                  onPressed: () {
+                    context.read<LocationCubit>().getLocation;
+                  }),
+            );
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }),
       ),
     );
   }
