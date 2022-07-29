5921414fe764d1896e34a750eafb6ce8dcd2d37a
Drunnb <drunnb@gmail.com>
2022-07-29 18:53:11 +0300

    start sqf project

--- stb_v1/lib/bloc/bird_post_cubit.dart
+++ stb_v1/lib/bloc/bird_post_cubit.dart
@@ -1,10 +1,6 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:stb_v1/models/bird_post_model.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 part 'bird_post_state.dart';
 
@@ -14,28 +10,9 @@ class BirdPostCubit extends Cubit<BirdPostState> {
 
   Future<void> loadPosts() async {
     emit(state.copyWith(status: BirdPostStatus.loading));
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<BirdModel> birdPosts = [];
-    final List<String>? birdPostJsonStringList =
-        prefs.getStringList('birdPosts');
 
-    if (birdPostJsonStringList != null) {
-      for (var postJsonData in birdPostJsonStringList) {
-        final Map<String, dynamic> data =
-            await json.decode(postJsonData) as Map<String, dynamic>;
-        final File imageFile = File(data['filePath'] as String);
-        final String birdName = data['birdName'] as String;
-        final String birdDescriprion = data['birdDescriprion'] as String;
-        final double latitude = data['latitude'] as double;
-        final double longitude = data['longitude'] as double;
-        birdPosts.add(BirdModel(
-            birdName: birdName,
-            latitude: latitude,
-            longitude: longitude,
-            birdDescriprion: birdDescriprion,
-            image: imageFile));
-      }
-    }
+    List<BirdModel> birdPosts = [];
+
     emit(state.copyWith(birdPosts: birdPosts, status: BirdPostStatus.loaded));
   }
 
@@ -47,8 +24,6 @@ class BirdPostCubit extends Cubit<BirdPostState> {
     birdPosts.add(
         birdModel); // добавляем к списку моделей, новую модель из функции (аргумент функции)
 
-    _saveToSharedPrefs(birdPosts);
-
     emit(state.copyWith(
         birdPosts: birdPosts,
         status: BirdPostStatus.loaded)); // меняем состояние на "загружено"
@@ -59,21 +34,6 @@ class BirdPostCubit extends Cubit<BirdPostState> {
     List<BirdModel> birdPosts = state.birdPosts;
 
     birdPosts.removeWhere((element) => element == birdModel);
-    _saveToSharedPrefs(birdPosts);
     emit(state.copyWith(birdPosts: birdPosts, status: BirdPostStatus.loaded));
-  }
-
-  Future<void> _saveToSharedPrefs(List<BirdModel> posts) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    final List<String> jsonDataList = posts
-        .map((post) => json.encode({
-              'birdName': post.birdName,
-              'birdDescriprion': post.birdDescriprion,
-              'latitude': post.latitude,
-              'longitude': post.longitude,
-              'filePath': post.image.path,
-            }))
-        .toList();
-    prefs.setStringList('birdPosts', jsonDataList);
   }
 }
