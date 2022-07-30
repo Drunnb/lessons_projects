068520222abc89a1bc03475bbde34389a47ebb40
Drunnb <drunnb@gmail.com>
2022-07-30 14:14:40 +0300

    test for working

--- stb_v1/lib/main.dart
+++ stb_v1/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          // Color.fromARGB(255, 54, 70, 92),
+          // Color.fromARGB(255, 54, 70, 92), dsada
           primarySwatch: Colors.blueGrey, // app bar color
           colorScheme: const ColorScheme.light().copyWith(
             primary: const Color.fromARGB(255, 71, 113, 134), // textField color
