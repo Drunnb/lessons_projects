dd1e3991a45333b13825a53c5236d656408f7eb6
Drunnb <drunnb@gmail.com>
2022-08-02 13:05:15 +0300

    create state for cubit

---
+++ social_media_app_v1/lib/bloc/auth_state.dart
@@ -0,0 +1,36 @@
+import 'package:equatable/equatable.dart';
+
+abstract class AuthState extends Equatable {
+  const AuthState();
+}
+
+class AuthInitial extends AuthState {
+  const AuthInitial();
+  @override
+  List<Object> get props => [];
+}
+
+class AuthLoading extends AuthState {
+  const AuthLoading();
+  @override
+  List<Object> get props => [];
+}
+
+class AuthSignedUp extends AuthState {
+  const AuthSignedUp();
+  @override
+  List<Object> get props => [];
+}
+
+class AuthSignedIn extends AuthState {
+  const AuthSignedIn();
+  @override
+  List<Object> get props => [];
+}
+
+class AuthFailure extends AuthState {
+  final String message;
+  const AuthFailure({required this.message});
+  @override
+  List<Object> get props => [];
+}
