4200c10cfce2bb65988bfbcb6edfec6739425ab7
Drunnb <drunnb@gmail.com>
2022-08-11 12:55:37 +0300

    2

--- gallery_app_v1/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ gallery_app_v1/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=F:\Dev\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\Dev\Projects\New\lessons_projects\lessons_projects\gallery_app_v1"
+export "FLUTTER_APPLICATION_PATH=F:\Dev\GitHub\lessons_projects-1\gallery_app_v1"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=.dart_tool/package_config.json"
