2dfc30ded7746df3314b72b3f50ba5b4ae95354b
Drunnb <drunnb@gmail.com>
2022-08-27 18:20:10 +0300

    end 3 part of project

--- flutter_fest/lib/application/UI/screens/main_tabs/main_tabs_screen.dart
+++ flutter_fest/lib/application/UI/screens/main_tabs/main_tabs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fest/resources/resources.dart';
 
 class MainTabsScreen extends StatelessWidget {
   const MainTabsScreen({super.key});
@@ -13,26 +14,58 @@ class MainTabsScreen extends StatelessWidget {
               color: Colors.white,
             ),
           ),
-          BottomNavigationBar(
-            currentIndex: 0,
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.calendar_month),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.dangerous),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: '',
-              ),
-            ],
-            onTap: (_) {},
-          ),
         ],
       ),
+      bottomNavigationBar: const ButtomBar(),
     );
+  }
+}
+
+class ButtomBar extends StatelessWidget {
+  const ButtomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final backgroundColor =
+        Theme.of(context).bottomNavigationBarTheme.backgroundColor ??
+            Colors.black;
+    return SizedBox(
+      height: 83,
+      child: ColoredBox(
+        color: backgroundColor,
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxHeight: 49,
+              maxWidth: 500,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 28),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: const [
+                  Expanded(child: _Item(imageName: AppImages.tabbarCalendar)),
+                  Expanded(child: _Item(imageName: AppImages.tabbarBookmark)),
+                  Expanded(child: _Item(imageName: AppImages.tabbarPoint)),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Item extends StatelessWidget {
+  final String imageName;
+  const _Item({
+    required this.imageName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(imageName);
   }
 }
