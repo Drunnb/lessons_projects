54ad589da29bbffb8919c16b2aa258b8f010b5b5
Drunnb <drunnb@gmail.com>
2022-08-29 19:27:54 +0300

    end 6 part

--- flutter_fest/lib/application/UI/screens/main_tabs/schedule_widget.dart
+++ flutter_fest/lib/application/UI/screens/main_tabs/schedule_widget.dart
@@ -6,48 +6,41 @@ class ScheduleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(AppImages.sheduleBg),
-          fit: BoxFit.none,
-          alignment: Alignment.topLeft,
-        ),
-      ),
-      child: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            const _LogoWidget(),
-            SliverPersistentHeader(
-              pinned: true,
-              delegate: _SliverAppBarDelegate(),
+    final topInset = MediaQuery.of(context).padding.top;
+
+    return SafeArea(
+      child: CustomScrollView(
+        slivers: [
+          const _LogoWidget(),
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: _SliverAppBarDelegate(topInset: topInset),
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            Container(color: Colors.pink, height: 150.0),
+            Container(color: Colors.cyan, height: 150.0),
+            Container(
+              color: Colors.white.withOpacity(0.0),
+              height: 150.0,
             ),
-            SliverList(
-                delegate: SliverChildListDelegate([
-              Container(color: Colors.pink, height: 150.0),
-              Container(color: Colors.cyan, height: 150.0),
-              Container(
-                color: Colors.white.withOpacity(0.0),
-                height: 150.0,
-              ),
-              Container(color: Colors.blue, height: 150.0),
-              Container(color: Colors.blue, height: 150.0),
-              Container(color: Colors.blue, height: 150.0),
-              Container(color: Colors.blue, height: 150.0),
-              Container(color: Colors.blue, height: 150.0),
-              Container(color: Colors.blue, height: 150.0),
-              Container(color: Colors.pink, height: 150.0),
-              Container(color: Colors.cyan, height: 150.0),
-              Container(color: Colors.indigo, height: 150.0),
-              Container(color: Colors.blue, height: 150.0),
-              Container(color: Colors.blue, height: 150.0),
-              Container(color: Colors.blue, height: 150.0),
-              Container(color: Colors.blue, height: 150.0),
-              Container(color: Colors.blue, height: 150.0),
-              Container(color: Colors.blue, height: 150.0),
-            ])),
-          ],
-        ),
+            Container(color: Colors.blue, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
+            Container(color: Colors.pink, height: 150.0),
+            Container(color: Colors.cyan, height: 150.0),
+            Container(color: Colors.indigo, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
+          ])),
+        ],
       ),
     );
   }
@@ -60,17 +53,22 @@ class _LogoWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
       child: SizedBox(
-        height: 200,
+        height: 204,
         child: Stack(
           children: [
             Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(AppImages.sheduleBg),
+            ),
+            Positioned(
               left: 20,
-              top: 40,
+              top: 84,
               child: Image.asset(AppImages.sheduleFfLogo),
             ),
             Positioned(
               right: 20,
-              top: 20,
+              top: 64,
               child: Image.asset(AppImages.sheduleSurfLogo),
             ),
           ],
@@ -82,12 +80,15 @@ class _LogoWidget extends StatelessWidget {
 
 class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
   final double _height = 56;
+  final double topInset;
+
+  _SliverAppBarDelegate({required this.topInset});
 
   @override
-  double get maxExtent => _height;
+  double get maxExtent => _height + topInset;
 
   @override
-  double get minExtent => _height;
+  double get minExtent => _height + topInset;
 
   @override
   Widget build(
@@ -95,7 +96,7 @@ class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
     double shrinkOffset,
     bool overlapsContent,
   ) {
-    return const _SectionsButtonsWidget();
+    return _SectionsButtonsWidget(topInset: topInset);
   }
 
   @override
@@ -105,50 +106,67 @@ class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
 }
 
 class _SectionsButtonsWidget extends StatelessWidget {
-  const _SectionsButtonsWidget();
+  final double topInset;
+  const _SectionsButtonsWidget({required this.topInset});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        scrollDirection: Axis.horizontal,
-        itemCount: 4,
-        itemBuilder: (context, index) {
-          return Center(
-            child: SizedBox(
-              height: 36,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                    padding: MaterialStateProperty.all(EdgeInsets.zero),
-                    elevation: MaterialStateProperty.all(0),
-                    minimumSize: MaterialStateProperty.all(Size.zero),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
+    const totalItem = 4;
+    const itemGradientWidth = 2;
+    const halfItemGradientPoint = itemGradientWidth / 2;
+    const endGradientPoint =
+        totalItem * itemGradientWidth + halfItemGradientPoint;
+    return ColoredBox(
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: ListView.separated(
+          padding: EdgeInsets.only(left: 20.0, right: 20, top: topInset),
+          scrollDirection: Axis.horizontal,
+          itemCount: 4,
+          itemBuilder: (context, index) {
+            final currentGradientStarPoint =
+                halfItemGradientPoint + index * itemGradientWidth;
+            final currentGradientEndPoint =
+                endGradientPoint - currentGradientStarPoint;
+            return Center(
+              child: SizedBox(
+                height: 36,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                      padding: MaterialStateProperty.all(EdgeInsets.zero),
+                      elevation: MaterialStateProperty.all(0),
+                      minimumSize: MaterialStateProperty.all(Size.zero),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                      )),
+                  child: Ink(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment(-currentGradientStarPoint, 0.0),
+                          end: Alignment(
+                              currentGradientEndPoint.toDouble(), 0.0),
+                          colors: const [Color(0xFF00BD13), Color(0xFF170AF4)],
+                        ),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(80.0)),
                       ),
-                    )),
-                child: Ink(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [Color(0xFF00BD13), Color(0xFF170AF4)],
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(80.0)),
-                    ),
-                    child: Center(
-                        child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text('Секция $index'),
-                    ))),
+                      child: Center(
+                          child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Text('Секция $index'),
+                      ))),
+                ),
               ),
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return const SizedBox(
-            width: 10,
-          );
-        });
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(
+              width: 10,
+            );
+          }),
+    );
   }
 }
