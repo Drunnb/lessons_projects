724533f75efbf502cad73d01217a81c9f31f46ad
Drunnb <drunnb@gmail.com>
2022-09-05 11:03:07 +0300

    end 14 part

--- flutter_fest/lib/application/UI/screens/main_tabs/schedule_widget.dart
+++ flutter_fest/lib/application/UI/screens/main_tabs/schedule_widget.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fest/application/UI/themes/app_colors.dart';
+import 'package:flutter_fest/application/UI/themes/app_text_style.dart';
 import 'package:flutter_fest/application/UI/widgets/schedule_row/schedule_row_break_widget.dart';
 import 'package:flutter_fest/application/UI/widgets/schedule_row/schedule_row_widget.dart';
+import 'package:flutter_fest/application/UI/widgets/top_notification/top_notification_overlay_widget.dart';
+import 'package:flutter_fest/application/UI/widgets/top_notification/top_text_notification_widget.dart';
 import 'package:flutter_fest/resources/resources.dart';
 
-class ScheduleWidget extends StatelessWidget {
+class ScheduleWidget extends StatefulWidget {
   const ScheduleWidget({super.key});
+
+  @override
+  State<ScheduleWidget> createState() => _ScheduleWidgetState();
+}
+
+class _ScheduleWidgetState extends State<ScheduleWidget> {
+  OverlayEntry? _lectionOverlay;
+
+  void showOverlay(BuildContext context) {
+    final overlay = _lectionOverlay;
+    if (overlay != null) {
+      overlay.remove();
+      _lectionOverlay = null;
+      return;
+    }
+    const textWidget =
+        TopTextNotificationWidget(text: 'Лекция добавлена в программу');
+    final entry = TopNotificationOvelayWidget.makeOverlayEntry(textWidget);
+    _lectionOverlay = entry;
+    Overlay.of(context)?.insert(entry);
+  }
 
   @override
   Widget build(BuildContext context) {
     final topInset = MediaQuery.of(context).padding.top;
-
     return SafeArea(
       child: CustomScrollView(
         slivers: [
@@ -18,6 +41,11 @@ class ScheduleWidget extends StatelessWidget {
           SliverPersistentHeader(
             pinned: true,
             delegate: _SliverAppBarDelegate(topInset: topInset),
+          ),
+          SliverToBoxAdapter(
+            child: ElevatedButton(
+                onPressed: () => showOverlay(context),
+                child: const Text('button')),
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
