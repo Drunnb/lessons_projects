1f19b5c90d635d9be45723aacee972e23e517f86
Drunnb <drunnb@gmail.com>
2022-09-07 23:06:01 +0300

    end 19 part

--- flutter_fest/lib/application/UI/screens/main_tabs/session_details_screen.dart
+++ flutter_fest/lib/application/UI/screens/main_tabs/session_details_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fest/application/UI/themes/app_colors.dart';
+import 'package:flutter_fest/application/UI/themes/app_text_style.dart';
+import 'package:flutter_fest/application/UI/themes/app_theme.dart';
 import 'package:flutter_fest/resources/resources.dart';
 
 class SessionDetailsScreen extends StatelessWidget {
@@ -7,9 +9,24 @@ class SessionDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      floatingActionButton: _AddToFavoriteWidget(),
+    return Scaffold(
+      floatingActionButton: const _AddToFavoriteWidget(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: maxScreenWidth,
+          ),
+          child: ListView(
+            children: const [
+              _HeaderWidget(),
+              _SessionTitleWidget(),
+              _SessionDiscriptionWidget(),
+              _ScheduleInfoWidget(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
@@ -60,44 +77,147 @@ class _AddToFavoriteWidget extends StatelessWidget {
     final configuration = isFavorite
         ? _AddToFavoriteWidgetConfiguration.favorite
         : _AddToFavoriteWidgetConfiguration.disFavorite;
-    return Container(
-      height: 40.0,
-      padding: const EdgeInsets.symmetric(horizontal: 40.0),
+    return SizedBox(
+      height: 48.0,
       child: Center(
         child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 300),
-          child: ElevatedButton(
-            onPressed: () {},
-            style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all(Colors.white),
-                padding: MaterialStateProperty.all(EdgeInsets.zero),
-                elevation: MaterialStateProperty.all(0),
-                minimumSize: MaterialStateProperty.all(Size.zero),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
+          constraints: const BoxConstraints(maxWidth: maxScreenWidth),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.white),
+                  padding: MaterialStateProperty.all(EdgeInsets.zero),
+                  elevation: MaterialStateProperty.all(0),
+                  minimumSize: MaterialStateProperty.all(Size.zero),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                  )),
+              child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: configuration.backgroundGradient,
+                    color: configuration.backgroundColor,
+                    borderRadius: const BorderRadius.all(Radius.circular(12.0)),
                   ),
-                )),
-            child: Ink(
-                decoration: BoxDecoration(
-                  gradient: configuration.backgroundGradient,
-                  color: configuration.backgroundColor,
-                  borderRadius: const BorderRadius.all(Radius.circular(12.0)),
-                ),
-                child: Center(
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Image.asset(configuration.icon,
-                          color: configuration.iconColor),
-                      const SizedBox(width: 16.0),
-                      Text(configuration.text),
-                    ],
-                  ),
-                )),
+                  child: Center(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset(configuration.icon,
+                            color: configuration.iconColor),
+                        const SizedBox(width: 16.0),
+                        Text(configuration.text),
+                      ],
+                    ),
+                  )),
+            ),
           ),
         ),
       ),
     );
+  }
+}
+
+class _HeaderWidget extends StatelessWidget {
+  const _HeaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: const [
+        _SpeakerInfoWidget(),
+      ],
+    );
+  }
+}
+
+class _SpeakerInfoWidget extends StatelessWidget {
+  const _SpeakerInfoWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 375 / 312,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Spacer(flex: 124),
+                Flexible(
+                  flex: 56,
+                  child: Text(
+                    'Алексей\nЧулпин',
+                    style: AppTextStyle.speakerName.copyWith(
+                      color: AppColors.white88,
+                    ),
+                  ),
+                ),
+                const Spacer(flex: 24),
+                Flexible(
+                  flex: 40,
+                  child: Text(
+                    'Ведущий\nразработчик МТС',
+                    style: AppTextStyle.bookTextItalic.copyWith(
+                      color: AppColors.white88,
+                    ),
+                  ),
+                ),
+                const Spacer(flex: 68),
+              ],
+            ),
+          ),
+          const Placeholder(),
+        ],
+      ),
+    );
+  }
+}
+
+class _SessionTitleWidget extends StatelessWidget {
+  const _SessionTitleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, top: 32.0, right: 20),
+      child: Text(
+        'Субьективность в оценке дизанайна ',
+        style: AppTextStyle.stainbeckText.copyWith(
+          color: AppColors.white88,
+        ),
+      ),
+    );
+  }
+}
+
+class _SessionDiscriptionWidget extends StatelessWidget {
+  const _SessionDiscriptionWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, top: 16.0, right: 20),
+      child: Text(
+        'Текст описание краткое лецкии. Что будем, раскрытие темы. Думаю, что на три или четыре строки, текста нет, так что пишу,что думаю бла бла бла...',
+        style: AppTextStyle.bookText.copyWith(
+          color: AppColors.white88,
+        ),
+      ),
+    );
+  }
+}
+
+class _ScheduleInfoWidget extends StatelessWidget {
+  const _ScheduleInfoWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
