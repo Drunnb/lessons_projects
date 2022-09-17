40ffff6b5977a859195be2fb76169fd6c5cd00d4
Drunnb <drunnb@gmail.com>
2022-09-17 16:29:15 +0300

    end 81 lesson

--- mvvm_counter/lib/domain/data_provider/auth_api_data_provider.dart
+++ mvvm_counter/lib/domain/data_provider/auth_api_data_provider.dart
@@ -4,6 +4,7 @@ class AuthApiProviderIncorectLoginDataError {}
 
 class AuthApiProvide {
   Future<String> login(String login, String password) async {
+    await Future<void>.delayed(const Duration(seconds: 1));
     final isSucsess = login == 'admin' && password == '123456';
     if (isSucsess) {
       return 'qwerty123456';
--- mvvm_counter/lib/ui/widgets/auth_widget.dart
+++ mvvm_counter/lib/ui/widgets/auth_widget.dart
@@ -1,14 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_counter/domain/data_provider/auth_api_data_provider.dart';
+import 'package:mvvm_counter/domain/services/auth_service.dart';
 import 'package:provider/provider.dart';
 
+enum _ViewModelAuthButtonState { canSubmit, authProcess, disable }
+
 class _ViewModelState {
-  final String authErrorTitle;
-  _ViewModelState({
-    required this.authErrorTitle,
-  });
+  String authErrorTitle = '';
+  String login = '';
+  String password = '';
+  bool isAuthInProcess = false;
+  _ViewModelAuthButtonState get authButtonState {
+    if (isAuthInProcess) {
+      return _ViewModelAuthButtonState.authProcess;
+    } else if (login.isNotEmpty && password.isNotEmpty) {
+      return _ViewModelAuthButtonState.canSubmit;
+    } else {
+      return _ViewModelAuthButtonState.disable;
+    }
+  }
+
+  _ViewModelState();
 }
 
-class _ViewModel extends ChangeNotifier {}
+class _ViewModel extends ChangeNotifier {
+  final _authService = AuthService();
+  final _state = _ViewModelState();
+  _ViewModelState get state => _state;
+
+  void changeLogin(String value) {
+    if (_state.login == value) return;
+    _state.login = value;
+    notifyListeners();
+  }
+
+  void changePassword(String value) {
+    if (_state.password == value) return;
+    _state.password = value;
+    notifyListeners();
+  }
+
+  Future<void> onAuthButtonPressed() async {
+    final login = _state.login;
+    final password = _state.password;
+
+    if (login.isEmpty || password.isEmpty) return;
+
+    _state.authErrorTitle = '';
+    _state.isAuthInProcess = true;
+
+    notifyListeners();
+
+    try {
+      await _authService.login(login, password);
+      _state.isAuthInProcess = false;
+      notifyListeners();
+    } on AuthApiProviderIncorectLoginDataError {
+      _state.authErrorTitle = 'Неправильный логин или пароль';
+      _state.isAuthInProcess = false;
+
+      notifyListeners();
+    } catch (exeption) {
+      _state.authErrorTitle = 'Что то пошло не так, попробуйте повторить позже';
+
+      notifyListeners();
+    }
+  }
+}
 
 class AuthWidget extends StatelessWidget {
   const AuthWidget({super.key});
@@ -21,7 +79,88 @@ class AuthWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+            child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            _ErrorTitleWidget(),
+            SizedBox(height: 10.0),
+            _LoginWidget(),
+            SizedBox(height: 10.0),
+            _PasswordWidget(),
+            SizedBox(height: 10.0),
+            AuthButtonWidget(),
+          ],
+        )),
+      ),
+    );
   }
 }
-// 40:09
+
+class _LoginWidget extends StatelessWidget {
+  const _LoginWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.read<_ViewModel>();
+    return TextField(
+      decoration: const InputDecoration(
+        labelText: 'Логин',
+        border: OutlineInputBorder(),
+      ),
+      onChanged: model.changeLogin,
+    );
+  }
+}
+
+class _PasswordWidget extends StatelessWidget {
+  const _PasswordWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.read<_ViewModel>();
+    return TextField(
+      decoration: const InputDecoration(
+        labelText: 'Пароль',
+        border: OutlineInputBorder(),
+      ),
+      onChanged: model.changePassword,
+    );
+  }
+}
+
+class _ErrorTitleWidget extends StatelessWidget {
+  const _ErrorTitleWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    final authErrorTitle =
+        context.select((_ViewModel value) => value.state.authErrorTitle);
+
+    return Text(authErrorTitle);
+  }
+}
+
+class AuthButtonWidget extends StatelessWidget {
+  const AuthButtonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.read<_ViewModel>();
+    final authButtonState =
+        context.select((_ViewModel value) => value.state.authButtonState);
+    final onPressed = authButtonState == _ViewModelAuthButtonState.canSubmit
+        ? model.onAuthButtonPressed
+        : null;
+    final child = authButtonState == _ViewModelAuthButtonState.authProcess
+        ? const CircularProgressIndicator()
+        : const Text('Авторизоваться');
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: child,
+    );
+  }
+}
