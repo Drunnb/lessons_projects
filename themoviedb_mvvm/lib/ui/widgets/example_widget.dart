d1374e3b08164154d4b0898043e61d2b0affe3c1
Drunnb <drunnb@gmail.com>
2022-09-19 12:59:45 +0300

    try to make it good

---
+++ themoviedb_mvvm/lib/ui/widgets/example_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ExampleWidget extends StatelessWidget {
+  const ExampleWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
---
+++ themoviedb_mvvm/lib/ui/widgets/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:themoviedb_mvvm/ui/widgets/example_widget.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ExampleWidget(),
+    );
+  }
+}
