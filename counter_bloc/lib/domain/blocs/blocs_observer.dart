e526af5acdc6af8864c4798740e1acc880ef1c47
Drunnb <drunnb@gmail.com>
2022-11-11 16:13:58 +0300

    end block counter with observer

---
+++ counter_bloc/lib/domain/blocs/blocs_observer.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+
+class BlocsObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    print(bloc);
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    print("${bloc}${event}");
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    print("with error: ${bloc} ${error} ${stackTrace}");
+  }
+}
--- counter_bloc/lib/main.dart
+++ counter_bloc/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:counter_bloc/domain/blocs/blocs_observer.dart';
 import 'package:counter_bloc/ui/widgets/my_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   const app = MyApp();
+  Bloc.observer = BlocsObserver();
+
   runApp(app);
 }
