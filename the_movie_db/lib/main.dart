890a7565bb75128fd38f4e99c678ee8afe67c331
Drunnb <drunnb@gmail.com>
2023-01-17 17:05:04 +0300

    last lesson with the movie db 106 DI

--- the_movie_db/lib/main.dart
+++ the_movie_db/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:the_movie_db/ui/widgets/app/my_app.dart';
 
+abstract class AppFactory {}
+
 void main() {
   const app = MyApp();
   runApp(app);
