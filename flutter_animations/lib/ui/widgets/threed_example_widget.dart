da2a1414e6c0d6af1a88c2212cce62049d160d67
Drunnb <drunnb@gmail.com>
2023-01-30 20:04:18 +0300

    end lesson 114 animatedList

--- flutter_animations/lib/ui/widgets/threed_example_widget.dart
+++ flutter_animations/lib/ui/widgets/threed_example_widget.dart
@@ -8,17 +8,29 @@ class ExampleWidget extends StatefulWidget {
 }
 
 class _ExampleWidgetState extends State<ExampleWidget> {
+  final _listKey = GlobalKey<AnimatedListState>();
   var _data = [1, 2, 3, 4];
   void _insert() {
     final element = (_data.isEmpty ? 0 : _data.last) + 1;
-    setState(() {
-      _data.add(element);
-    });
+    _data.add(element);
+    final index = _data.length - 1;
+    _listKey.currentState?.insertItem(index);
   }
 
   void _removeAtIndex(int index) {
-    setState(() {
-      _data.removeAt(index);
+    final element = _data.removeAt(index);
+    _listKey.currentState?.removeItem(index, (context, animation) {
+      return SlideTransition(
+        position: animation.drive(Tween<Offset>(
+          begin: const Offset(1, 0),
+          end: const Offset(0, 0),
+        )),
+        child: Container(
+          width: double.infinity,
+          height: 40,
+          color: Colors.redAccent,
+        ),
+      );
     });
   }
 
@@ -30,22 +42,40 @@ class _ExampleWidgetState extends State<ExampleWidget> {
         child: const Icon(Icons.add),
       ),
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: _data.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () => _removeAtIndex(index),
-              child: SizedBox(
-                width: double.infinity,
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('${_data[index]}'),
-                  ),
-                ),
+        child: AnimatedList(
+          key: _listKey,
+          initialItemCount: _data.length,
+          itemBuilder: (context, index, animation) {
+            return SizeTransition(
+              sizeFactor: animation,
+              child: GestureDetector(
+                onTap: () => _removeAtIndex(index),
+                child: _RowWidget(text: _data[index]),
               ),
             );
           },
+        ),
+      ),
+    );
+  }
+}
+
+class _RowWidget extends StatelessWidget {
+  const _RowWidget({
+    super.key,
+    required this.text,
+  });
+
+  final int text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("$text"),
         ),
       ),
     );
