2203582764e5bbf8f3f0b1323b684f09863f9851
Drunnb <drunnb@gmail.com>
2023-02-02 19:55:00 +0300

    end 115 lesson animation  SomeTransition

--- flutter_animations/lib/ui/widgets/fourth_example_widget.dart
+++ flutter_animations/lib/ui/widgets/fourth_example_widget.dart
@@ -9,10 +9,16 @@ class ExampleWidget extends StatefulWidget {
   State<ExampleWidget> createState() => _ExampleWidgetState();
 }
 
-class _ExampleWidgetState extends State<ExampleWidget> {
+class _ExampleWidgetState extends State<ExampleWidget>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
   @override
   void initState() {
     super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: duration,
+    );
   }
 
   @override
@@ -21,7 +27,13 @@ class _ExampleWidgetState extends State<ExampleWidget> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(
-            () {},
+            () {
+              if (_controller.isCompleted) {
+                _controller.reverse();
+              } else {
+                _controller.forward();
+              }
+            },
           );
         },
         child: const Icon(Icons.play_arrow),
@@ -31,11 +43,10 @@ class _ExampleWidgetState extends State<ExampleWidget> {
           child: Container(
             padding: const EdgeInsets.all(40.0),
             color: Colors.black,
-            child: Container(
-              width: 100,
-              height: 200,
-              color: Colors.red,
-            ),
+            // child: ScaleTransitionExample(
+            //   controller: _controller,
+            // )),
+            child: AnimatedWidgetExample(controller: _controller),
           ),
         ),
       ),
@@ -43,14 +54,48 @@ class _ExampleWidgetState extends State<ExampleWidget> {
   }
 }
 
-class AnimatedWidgetExample extends AnimatedWidget {
-  const AnimatedWidgetExample(
-      {super.key, required AnimationController controller})
+class ScaleTransitionExample extends AnimatedWidget {
+  final AnimationController controller;
+  const ScaleTransitionExample({super.key, required this.controller})
       : super(listenable: controller);
 
   @override
   Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        // return child ?? const SizedBox.shrink();
+        return Transform.scale(
+          scale: controller.value,
+          child: child,
+        );
+      },
+      child: Container(
+        width: 100,
+        height: 200,
+        color: Colors.red,
+      ),
+    );
+
+    // ScaleTransition(
+    //   scale: controller,
+    //   child: Container(
+    //     width: 100,
+    //     height: 200,
+    //     color: Colors.red,
+    //   ),
+    // );
+  }
+}
+
+class AnimatedWidgetExample extends AnimatedWidget {
+  const AnimatedWidgetExample(
+      {super.key, required AnimationController controller})
+      : super(listenable: controller);
+  @override
+  Widget build(BuildContext context) {
     final animation = listenable as Animation<double>;
+
     return Transform.scale(
       scale: animation.value,
       child: Container(
