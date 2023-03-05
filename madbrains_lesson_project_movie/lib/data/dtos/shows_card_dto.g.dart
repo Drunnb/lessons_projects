4d66e4b86d85ec73bcc99b48c11fb9295bb35765
Drunnb <drunnb@gmail.com>
2023-03-05 18:25:29 +0300

    add DTO models for parsing json

---
+++ madbrains_lesson_project_movie/lib/data/dtos/shows_card_dto.g.dart
@@ -0,0 +1,77 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shows_card_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ShowsCardDTO _$ShowsCardDTOFromJson(Map<String, dynamic> json) => ShowsCardDTO(
+      id: json['id'] as int? ?? 0,
+      url: json['url'] as String,
+      title: json['name'] as String? ?? '',
+      genres: (json['genres'] as List<dynamic>)
+          .map((e) => $enumDecode(_$GenreEnumMap, e))
+          .toList(),
+      releaseDate: json['premiered'] as String?,
+      averegeVoite: Rating.fromJson(json['rating'] as Map<String, dynamic>),
+      image: json['image'] == null
+          ? null
+          : ShowsCardDTOImage.fromJson(json['image'] as Map<String, dynamic>),
+      description: json['summary'] as String?,
+    );
+
+Map<String, dynamic> _$ShowsCardDTOToJson(ShowsCardDTO instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'url': instance.url,
+      'name': instance.title,
+      'genres': instance.genres.map((e) => _$GenreEnumMap[e]!).toList(),
+      'premiered': instance.releaseDate,
+      'rating': instance.averegeVoite.toJson(),
+      'image': instance.image?.toJson(),
+      'summary': instance.description,
+    };
+
+const _$GenreEnumMap = {
+  Genre.DRAMA: 'DRAMA',
+  Genre.SCIENCE_FICTION: 'SCIENCE_FICTION',
+  Genre.THRILLER: 'THRILLER',
+  Genre.ACTION: 'ACTION',
+  Genre.CRIME: 'CRIME',
+  Genre.HORROR: 'HORROR',
+  Genre.ROMANCE: 'ROMANCE',
+  Genre.ADVENTURE: 'ADVENTURE',
+  Genre.ESPIONAGE: 'ESPIONAGE',
+  Genre.MUSIC: 'MUSIC',
+  Genre.MYSTERY: 'MYSTERY',
+  Genre.SUPERNATURAL: 'SUPERNATURAL',
+  Genre.FANTASY: 'FANTASY',
+  Genre.FAMILY: 'FAMILY',
+  Genre.ANIME: 'ANIME',
+  Genre.COMEDY: 'COMEDY',
+  Genre.HISTORY: 'HISTORY',
+  Genre.MEDICAL: 'MEDICAL',
+  Genre.LEGAL: 'LEGAL',
+  Genre.WESTERN: 'WESTERN',
+  Genre.WAR: 'WAR',
+  Genre.SPORTS: 'SPORTS',
+};
+
+Rating _$RatingFromJson(Map<String, dynamic> json) => Rating(
+      average: (json['average'] as num?)?.toDouble() ?? 0,
+    );
+
+Map<String, dynamic> _$RatingToJson(Rating instance) => <String, dynamic>{
+      'average': instance.average,
+    };
+
+ShowsCardDTOImage _$ShowsCardDTOImageFromJson(Map<String, dynamic> json) =>
+    ShowsCardDTOImage(
+      original: json['original'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$ShowsCardDTOImageToJson(ShowsCardDTOImage instance) =>
+    <String, dynamic>{
+      'original': instance.original,
+    };
