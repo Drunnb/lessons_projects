a30bbdd1682f894e0694af86bce1a33af540df03
Drunnb <drunnb@gmail.com>
2023-05-04 17:27:38 +0300

    end 8 part

--- bloc_counter_again/lib/src/search_page.dart
+++ bloc_counter_again/lib/src/search_page.dart
@@ -1,8 +1,11 @@
-import 'package:bloc_counter_again/src/bloc/search_bloc/search_bloc.dart';
-import 'package:bloc_counter_again/src/bloc/search_bloc/search_event.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:search_user_repository/search_user_repository.dart';
+
+import 'package:bloc_counter_again/src/bloc/search_bloc/search_bloc.dart';
+import 'package:bloc_counter_again/src/bloc/search_bloc/search_event.dart';
 
 class SearhPage extends StatelessWidget {
   const SearhPage({super.key});
@@ -61,14 +64,89 @@ class MainSearchWidget extends StatelessWidget {
           Expanded(
             child: ListView.builder(
               itemBuilder: (context, index) {
+                final user = users[index];
                 return ListTile(
-                  title: Text(users[index].userName ?? ''),
+                  title: Text(user.userName ?? ''),
+                  leading: Hero(
+                    tag: user.userName ?? '',
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(user.images ?? ''),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => UserInfoScreen(
+                          user: user,
+                        ),
+                      ),
+                    );
+                  },
                 );
               },
               itemCount: users.length,
             ),
           ),
       ],
+    );
+  }
+}
+
+class UserInfoScreen extends StatelessWidget {
+  final UserModel user;
+  const UserInfoScreen({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          user.userName ?? '',
+          style: const TextStyle(
+            fontSize: 22,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Hero(
+            tag: user.userName ?? '',
+            child: Container(
+              width: double.infinity,
+              height: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(user.images ?? ''),
+                ),
+              ),
+            ),
+          ),
+          Text.rich(
+            TextSpan(
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+              children: [
+                const TextSpan(text: 'Visit site '),
+                TextSpan(
+                  text: user.url ?? '',
+                  style: const TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                  recognizer: TapGestureRecognizer()..onTap = () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
