3945abed7cd8be924c9ec2af09b7c84a99ba6c7b
Drunnb <drunnb@gmail.com>
2023-07-11 13:35:04 +0300

    try to fix work

--- homeWork/dart_application_1_test/bin/dart_application_1_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dart_application_1_test/dart_application_1_test.dart'
-    as dart_application_1_test;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1_test.calculate()}!');
-
-  final joke =
-      dart_application_1_test.JokeRepository().getFiveRandomPoems().then(print);
-}
--- homeWork/dart_application_1_test/test/dart_application_1_test_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_application_1_test/dart_application_1_test.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
